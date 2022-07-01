9867d0b8add52505138f6cd69d53654f4a1931c8
x10-Tor <withan.w@allrightbox.com>
2022-07-01 14:48:51 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const res = require("express/lib/response");
+const cors = require("cors");
+const app = express();
+const port = 8001;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(`APP Worked on port ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
